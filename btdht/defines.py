c1a908f0cb1cb268207cdaa1d042391f7de5a518
INVENT <invent@0x0a.net>
2015-08-04 11:36:55 +0300

    Config changes

--- btdht/defines.py
+++ btdht/defines.py
@@ -1,8 +1,19 @@
+
+# How many DHT nodes we query per ITERATION_TIMEOUT
 SAMPLE_COUNT = 8
+
+# How often we query DHT nodes
 ITERATION_TIMEOUT = 2
+
+# Exit if error count > then these
 MAX_BOOTSTRAP_ERRORS = 5
+
+# Searching for dead nodes and cleanup them every GC_MAX_TIME
 GC_MAX_TIME = 60
 GX_MAX_TRANS = 5
 
+# If True, we start to generate random node_id every ITERATION_TIMEOUT. This can greatly increase speed of DHT node harvesting, but slows down BT peers harvesting.
 RANDOMIZE_NODE_ID = False
+
+# If True, we start to send find_peers message to every known DHT node instead of SAMPLE_COUNT. This can greatly increase speed of DHT node harvesting, but slows down BT peers harvesting.
 RANDOM_FIND_PEERS = False
--- get_hash_peers.py
+++ get_hash_peers.py
@@ -8,20 +8,20 @@ from btdht import DHT
 
 current_magnet = "746385fe32b268d513d068f22c53c46d2eb34a5c".decode("hex")
 
+# Enable logging at DEBUG level
+loglevel = logging.DEBUG
+formatter = logging.Formatter("[%(levelname)s@%(created)s] %(message)s")
+stdout_handler = logging.StreamHandler()
+stdout_handler.setFormatter(formatter)
+logging.getLogger("btdht").setLevel(loglevel)
+logging.getLogger("btdht").addHandler(stdout_handler)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(loglevel)
+logger.addHandler(stdout_handler)
+
 if __name__ == "__main__":
 
-    # Enable logging at DEBUG level
-    loglevel = logging.DEBUG
-    formatter = logging.Formatter("[%(levelname)s@%(created)s] %(message)s")
-    stdout_handler = logging.StreamHandler()
-    stdout_handler.setFormatter(formatter)
-    logging.getLogger("btdht").setLevel(loglevel)
-    logging.getLogger("btdht").addHandler(stdout_handler)
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(loglevel)
-    logger.addHandler(stdout_handler)
- 
     # Start DHT Node on port 60000
     dht = DHT(host='0.0.0.0', port=60000)
     dht.start()
@@ -31,8 +31,8 @@ if __name__ == "__main__":
 
     # Find me peers for that torrent hashes
     dht.ht.add_hash(current_magnet)
-    TotalCnt = 0
-    Res = []
+    
+    res = []
     for count in xrange(10):
         logger.info("DHT Nodes found: %d" % (dht.rt.count()))
         logger.info("Bad DHT nodes found: %d" % (dht.rt.bad_count()))
@@ -41,10 +41,10 @@ if __name__ == "__main__":
         # How many peers at this moment?
         peers = dht.ht.get_hash_peers(current_magnet)
         for peer in peers:
-            TotalCnt += 1
-            Res.append((peer))
+            res.append((peer))
             logger.info("Found peer: %s:%d" % (peer))
+
         time.sleep(3)
-    logger.info("Found peers (total): %d" % (TotalCnt))
-    logger.info("Found peers (uniq): %d" % (len(set(Res))))
+    logger.info("Found peers (total): %d" % (len(res)))
+    logger.info("Found peers (uniq): %d" % (len(set(res))))
     dht.stop()
